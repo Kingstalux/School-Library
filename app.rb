f54407cb8fab111a6666feb852bc29f994ffb54b
Kingstalux <kingsleyngu25@gmail.com>
2022-01-21 19:05:37 +0100

    Fix linters

--- app.rb
+++ app.rb
@@ -3,39 +3,39 @@ require './manage_books'
 require './manage_rentals'
 
 def sub_app(number)
-    case number
-    when '1'
-      list_books
-  
-    when '2'
-      display_person
-  
-    when '3'
-      create_person
-  
-    when '4'
-      create_book
-    end
+  case number
+  when '1'
+    list_books
+
+  when '2'
+    display_person
+
+  when '3'
+    create_person
+
+  when '4'
+    create_book
   end
-  
-  def sub_app2(number)
-    case number
-    when '5'
-      create_rental
-  
-    when '6'
-      list_rentals
-  
-    when '7'
-      store_books
-      store_students
-      store_teachers
-      store_rentals
-      exit_msg
-    end
+end
+
+def sub_app2(number)
+  case number
+  when '5'
+    create_rental
+
+  when '6'
+    list_rentals
+
+  when '7'
+    store_books
+    store_students
+    store_teachers
+    store_rentals
+    exit_msg
   end
-  
-  def app(number)
-    sub_app(number)
-    sub_app2(number)
-  end
+end
+
+def app(number)
+  sub_app(number)
+  sub_app2(number)
+end
--- manage_books.rb
+++ manage_books.rb
@@ -2,71 +2,71 @@ require './book'
 require 'json'
 
 def initialize_book
-    book_file = './books.json'
-    f = File.read(book_file)
-    if f.empty? == false
-      json = JSON.parse(f)
-      get_book(json)
-    else
-      @book_arr = []
-    end
+  book_file = './books.json'
+  f = File.read(book_file)
+  if f.empty? == false
+    json = JSON.parse(f)
+    get_book(json)
+  else
+    @book_arr = []
+  end
 end
 
 def get_book(json)
-    @book_arr = []
-    i = 0
-    while i < json.length
-      b = Book.new(json[i]['title'], json[i]['author'])
-      @book_arr.push(b)
-      i += 1
-    end
+  @book_arr = []
+  i = 0
+  while i < json.length
+    b = Book.new(json[i]['title'], json[i]['author'])
+    @book_arr.push(b)
+    i += 1
+  end
 end
 
 def create_book
-    print 'Title: '
-    title = gets.chomp
-    print 'Author: '
-    author = gets.chomp
-    book = Book.new(title, author)
-    @book_arr.push(book)
-    puts 'Book created successfully'
-    puts ' '
-    main
-    @book_arr
-  end
+  print 'Title: '
+  title = gets.chomp
+  print 'Author: '
+  author = gets.chomp
+  book = Book.new(title, author)
+  @book_arr.push(book)
+  puts 'Book created successfully'
+  puts ' '
+  main
+  @book_arr
+end
 
-  def list_books
-    @book_arr.each { |x| puts "Title: '#{x.title}', Author: #{x.author}" }
-    puts ' '
-    main
-  end
+def list_books
+  @book_arr.each { |x| puts "Title: '#{x.title}', Author: #{x.author}" }
+  puts ' '
+  main
+end
 
-  def select_books
-    puts 'Select a book from the following list by number'
-    @book_arr.each { |x| puts "#{@book_arr.find_index(x)}) Title: #{x.title}, Author: #{x.author}" }
-  end
+def select_books
+  puts 'Select a book from the following list by number'
+  @book_arr.each { |x| puts "#{@book_arr.find_index(x)}) Title: #{x.title}, Author: #{x.author}" }
+end
 
-  def store_books
-    books_json = []
-    @book_arr.each do |book|
-        b = {
-            'title' => book.title,
-            'author' => book.author
-          }
-      books_json.push(b)
+def store_books
+  books_json = []
+  @book_arr.each do |book|
+    b = {
+      'title' => book.title,
+      'author' => book.author
+    }
+    books_json.push(b)
+  end
+  File.write('./books.json', books_json.to_json)
+end
+
+def take_book_title(title)
+  i = 0
+  tmp = -1
+  while i < @book_arr.length
+    if @book_arr[i].title == title
+      tmp = @book_arr[i]
+      break
     end
-    File.write('./books.json', books_json.to_json)
+    i += 1
   end
-
-  def take_book_title(title)
-    i = 0
-    tmp = -1
-    while i < @book_arr.length
-      if @book_arr[i].title == title
-        tmp = @book_arr[i]
-        break
-      end
-      i += 1
-    end
-    tmp
-  end
+  tmp
+end
